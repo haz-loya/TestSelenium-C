fb8e5fc971ff4feea3909d28125e6ebd9f268ce9
haz_loya <heloya92@gmail.com>
2021-11-30 18:06:50 -0600

    added proyect

---
+++ FacebookCreateNew/FacebookCreateNew/Drivers/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace HelloWorld.Drivers
+{
+   public class EntryPoint
+    {
+        WebDriver driver;
+        By CreateNewButton = By.XPath("//*[@data-testid='open - registration - form - button']");
+        public void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+            Thread.Sleep(3000);
+
+        }
+
+        public  Boolean ClickOnCreateNewAccount()
+        {
+            driver.FindElement(CreateNewButton).Click();
+            return true;
+        }
+
+        public static void clickOnButton(string button)
+        {
+            switch(button)
+            {
+                case "Create new account":
+                
+                break;
+            }
+        }
+
+    }
+}
---
+++ FacebookCreateNew/FacebookCreateNew/Features/Login.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login
+
+@Login
+Scenario: User tries to create a new facebook account without email
+	Given User clicks on "Create new account" button
+	And User adds the following new user data:
+	| FirstName | LastName | Email | Birthday    | Gender |
+	| Jorge     | Sanchez  |       | Jul/12/1986 | Male   |
+	When User clicks on "Sign Up" button
+	Then User validates the error message "You'll use this when you log in and if you ever need to reset your password"
