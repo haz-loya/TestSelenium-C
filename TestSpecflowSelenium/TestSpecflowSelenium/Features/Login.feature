0cf44a5b0aefc764b3ff73fba0dd88b34ca68fc0
haz-loya <heloya92@gmail.com>
2021-12-01 18:13:15 -0600

    Added testcase

---
+++ TestSpecflowSelenium/TestSpecflowSelenium/Features/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+
+@Login
+Scenario: User tries to create a new facebook account without email
+	Given User navigates to Facebook using "Chrome"
+	Given User clicks on "Create new account" button
+	And User adds the following new user data:
+	| FirstName | LastName | Email | Birthday    | Gender |
+	| Jorge     | Sanchez  |       | Jul/12/1986 | Male   |
+	When User clicks on "Sign Up" button
+	Then User validates Warining sign on "" textbox
---
+++ TestSpecflowSelenium/TestSpecflowSelenium/Hooks/Hookinitialization.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TestSpecflowSelenium.Drivers;
+
+namespace TestSpecflowSelenium.Hooks
+{
+    [Binding]
+    public sealed class Hookinitialization
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public Hookinitialization(ScenarioContext scenarioContext) => _scenarioContext = scenarioContext;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            SeleniumDriver seleniumDriver = new SeleniumDriver(_scenarioContext);
+            _scenarioContext.Set(seleniumDriver, "SeleniumDriver");
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Console.WriteLine("Selenium Webdriver quit");
+            _scenarioContext.Get<IWebDriver>("WebDriver").Quit();
+        }
+    }
+}
